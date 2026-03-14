be945eae1ba030b4befcfab94810995b5faf2d60
kristinayungman <alyasezer778@gmail.com>
2026-03-14 18:27:20 +0500

    add library class

---
+++ GeometryShapee/Restangle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryShapee
+{
+    public class Restangle
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Restangle(double width, double height)
+        {
+            if (width <= 0 || height <= 0)
+            {
+                throw new ArgumentException("Неккоректный ввод!");
+            }
+
+            Width = width;
+            Height = height;
+        }
+
+        public double Area()
+        {
+            return Width * Height;
+        }
+
+        public double Perimeter()
+        {
+            return 2 * (Width + Height);
+        }
+
+        public string ToString()
+        {
+            return $"Прямоугольник: Ширина = {Width}, Высота = {Height}, Площадь = {Area()}, Периметр = {Perimeter()}";
+        }
+
+
+
+    }
+}
---
+++ GeometryShapee/Triangle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryShapee
+{
+    public class Triangle
+    {
+        public double SideA { get; set; }
+        public double SideB { get; set; }
+        public double SideC { get; set; }
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            SideA = sideA;
+            SideB = sideB;
+            SideC = sideC;
+            if (SideA + SideB <= SideC)
+            {
+                throw new ArgumentException("Такой треугольник не может существовать!");
+            }
+            if (SideA + SideC <= SideB)
+            {
+                throw new ArgumentException("Такой треугольник не может существовать!");
+            }
+            if (SideC + SideB <= SideA)
+            {
+                throw new ArgumentException("Такой треугольник не может существовать!");
+            }
+        }
+        public double Perimeter()
+        {
+            return SideA + SideB + SideC;
+        }
+        public double Area()
+        {
+            return Math.Sqrt((Perimeter() / 2) * ((Perimeter() / 2) - SideA) * ((Perimeter() / 2) - SideB) * ((Perimeter() / 2) - SideC));
+        }
+        public string ToString()
+        {
+            return $"Треугольник: Первая сторона = {SideA}, Вторая сторона = {SideB}, Третья сторона = {SideC},\n Периметр = {Perimeter()} , Площадь = {Area()}";
+        }
+
+    }
+}
